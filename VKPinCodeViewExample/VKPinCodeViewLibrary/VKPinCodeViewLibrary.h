e59d891c1e6fb790b3fba7d93f42b466107ed37d
Vladimir <vladimir.kokhanevich@gmail.com>
2019-02-24 13:13:28 +0300

    Add framework target

---
+++ VKPinCodeViewExample/VKPinCodeViewLibrary/VKPinCodeViewLibrary.h
@@ -0,0 +1,19 @@
+//
+//  VKPinCodeViewLibrary.h
+//  VKPinCodeViewLibrary
+//
+//  Created by Vladimir Kokhanevich on 24/02/2019.
+//  Copyright © 2019 Vladimir Kokhanevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VKPinCodeViewLibrary.
+FOUNDATION_EXPORT double VKPinCodeViewLibraryVersionNumber;
+
+//! Project version string for VKPinCodeViewLibrary.
+FOUNDATION_EXPORT const unsigned char VKPinCodeViewLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKPinCodeViewLibrary/PublicHeader.h>
+
+
